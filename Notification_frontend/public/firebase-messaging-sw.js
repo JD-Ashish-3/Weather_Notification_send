66138c57d57f0d5a6c18b8e7c52088c292071a82
JD-Ashish-3 <ashishba33@gmail.com>
2025-07-16 19:05:51 +0530

    updated

--- Notification_frontend/public/firebase-messaging-sw.js
+++ Notification_frontend/public/firebase-messaging-sw.js
@@ -1,13 +1,13 @@
-// public/firebase-messaging-sw.js
+
 self.importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js');
 self.importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
 
-const apiKey = import.meta.env.VITE_API_KEY;
-const authDomain = import.meta.env.VITE_AUTH_DOMAIN;
-const projectId = import.meta.env.VITE_PROJECT_ID;
-const storageBucket = import.meta.env.VITE_STORAGE_BUCKET;
-const messagingSenderId = import.meta.env.VITE_MESSAGING_SENDER_ID;
-const appId = import.meta.env.VITE_APP_ID;
+const apiKey = "AIzaSyAg-gbrNLJ94pC6K_GEP29aHULha456iDM";
+const authDomain = "notification-send-dd463.firebaseapp.com";
+const projectId = "notification-send-dd463";
+const storageBucket = "notification-send-dd463.appspot.com";
+const messagingSenderId = "757470683851";
+const appId = "1:757470683851:android:534da78f188bd6d55b314e";
 
 firebase.initializeApp({
   apiKey: apiKey, 
